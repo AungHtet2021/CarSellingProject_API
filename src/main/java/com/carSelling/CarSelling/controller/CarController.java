3c7bac1e168da8ae4fbea331a34ccebfb22f756e
Aung Htet <aunghtetmyanmar2021@gmail.com>
2022-12-30 12:53:08 +0630

    test3

--- src/main/java/com/carSelling/CarSelling/controller/CarController.java
+++ src/main/java/com/carSelling/CarSelling/controller/CarController.java
@@ -30,29 +30,13 @@ public class CarController {
 	@Autowired
 	CarService carService;
 	
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 	  @Autowired
 	    CarRepository carRepository;
+	 
 	  
 
-=======
-	  @Autowired
-	    CarRepository carRepository;
-	  
->>>>>>> 3dc2a04b67164bfe3bad20f08b5d4bc8234cb0ef
-=======
-	  @Autowired
-	    CarRepository carRepository;
-	  
->>>>>>> 3dc2a04b67164bfe3bad20f08b5d4bc8234cb0ef
-=======
-	  @Autowired
-	    CarRepository carRepository;
-	  
->>>>>>> 3dc2a04b67164bfe3bad20f08b5d4bc8234cb0ef
 	@Autowired
 	StorageService storageService;
 	
